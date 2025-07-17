bcda160029e8bc965bacd96e6809c0a19d730b6b
ankur-upflairs <ankur.upflairs@gmail.com>
2025-07-17 16:16:58 +0530

    fjd

--- java script/array.js
+++ java script/array.js
@@ -1,15 +1,15 @@
-//array => collection of data types 
-let arr = ['hello',true,34]
+//array => collection of data types
+let arr = ["hello", true, 34];
 // console.log(arr.at(2))
 // console.log(arr[1])
-// console.log(arr.length) 
-//create an array of 5 element by taking value from user 
+// console.log(arr.length)
+//create an array of 5 element by taking value from user
 //and print each value using loop
 
 // let a1 = prompt('enter element')
 // let a2 = prompt('enter element')
 
-// let arr1 = [a1,a2] 
+// let arr1 = [a1,a2]
 // for(let i = 0 ; i<arr1.length;i++){
 //     arr1[i]
 // }
@@ -22,7 +22,7 @@ let arr = ['hello',true,34]
 // let b =a.fill(1,0,3)
 // let b = a.includes(5)
 // let b = a.reverse()
-// let b =a.slice(1,4) 
+// let b =a.slice(1,4)
 
 // console.log(`orginal array=>${a} \n method return =>${b}`)
 // for(let i in a){
@@ -33,33 +33,72 @@ let arr = ['hello',true,34]
 // }
 //delete an element from an array at specific position
 //insert an element from an array at specific position
-let a = [ 2,3,5,6,8,1]
+// let a = [2, 3, 5, 6, 8, 1];
 // a.forEach((value,index,array)=>{
 //     console.log(value,index,array)
 // })
 // console.log()
-let double = (v,i)=>{
-    return v*2
-}
-let b = a.map(double)
+// let double = (v, i) => {
+//   return v * 2;
+// };
+// let b = a.map(double);
 
-console.log(b)
+// console.log(b)
 // make function that create loop over an array and take an cb fnc
 // the cb is called on every iteration and whatever it return
 //main function is store all values in an array and return it
 
-
 // function MyMap(arr,cb_fnc){
 //     let newArr = []
 //     for(let i =0;i<arr.length ;i++){
-//        let y = cb_fnc(arr[i],i,arr) 
+//        let y = cb_fnc(arr[i],i,arr)
 //        newArr.push(y)
 //     }
 //     return newArr
 // }
+// How do you find all pairs of elements in an array whose sum equals a target number?
+// Example: [1, 2, 3, 4, 5], target = 6 → [[1,5], [2,4]]
+// let x = [1, 2, 3, 4, 5];
+// let y = [];
+// for (let i =0;i<x.length;i++) {
+//     // console.log(i)
+//   for (let j = i + 1; j < x.length; j++) {
+//       console.log(i, j);
+//     if ((x[i] + x[j]) == 6) {
+//       y.push([x[i], x[j]]);
+//     }
+//   }
+// }
+// console.log(y);
 
+let a = [2, 3, 5,12, 6, 8, 1];
+// console.log(a.every((v,i)=>{
+//     return v < 7 
+// }))
 
+// console.log(a.some((v,i)=>{
+//     return v < 7 
+// }))
 
+// let b = a.filter((v,i)=>v<=5)
+// console.log(b)
+// console.log(a.filter((v,i)=>i!=2))
+// let c = [[2,3],[5,6]]
+// console.log(c.flat(Infinity))
+
+// console.log(a.find(v=>v>7))
+// console.log(a.findIndex(v=>v>4))
+// console.log(a.sort((a,b)=>a-b)) 
+
+// a.splice(2,0,34,56)
+// console.log(a)
+
+// console.log(a.reduce((prev,current,index,array)=>{
+//     return prev + current 
+// },0))
+console.log(Array.isArray(a))
+//write a function to flatten an array depth => infinite
+// make function to calculate multiplication of array elements (reduce) 
 
 
 
---
+++ java script/object.js
@@ -0,0 +1,25 @@
+let person = {
+    name:'Gautam',
+    age:20,
+    "roll no":112
+}
+let key ="roll no"
+// console.log(person["roll no"])
+// console.log(person[key])
+
+// for(let i in person){
+//     console.log(i,person[i])
+// }
+// console.log(person.hasOwnProperty("name"))
+// for (const key in person) {
+//     if (person.hasOwnProperty( key)) {
+//         const element = object[key];        
+//     }
+// }
+
+// console.log(Object.values(person))
+
+// for(let i of Object.keys(person) ){
+//     console.log(i)
+// }
+// console.log(Object.entries(person))
