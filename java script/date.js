8623dd6615cc8e05e328e864713916c1381a7a8a
ankur-upflairs <ankur.upflairs@gmail.com>
2025-07-23 16:20:58 +0530

    code pushed

---
+++ java script/date.js
@@ -0,0 +1,19 @@
+let date = new Date()
+// console.log(date)
+// console.log(date.getMonth())
+// console.log(date.setFullYear(2000))
+// console.log(date)
+
+// let dob = new Date(2002,0,2)
+// console.log(dob)
+
+// console.log(Math.cbrt(64))
+// console.log(Math.pow(4,4))
+// console.log(Math.random()) 
+
+//make a function to generaate random no b/w 20 to 100 
+
+
+
+
+
