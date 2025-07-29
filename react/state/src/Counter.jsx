4cbc8be3956f3edcec6759de3650b147488ed03a
ankur-upflairs <ankur.upflairs@gmail.com>
2025-07-29 11:32:11 +0530

    code pushed

--- react/state/src/Counter.jsx
+++ react/state/src/Counter.jsx
@@ -1,18 +1,20 @@
 import React, { useState } from 'react'
 
-function Counter() {
-    //state is memory of comp
-    //when state changes component re-renders
-    //state is immutable u can not change state directly
-    //use setter fn to change state 
-    //it preserve the value b/w re-renders 
+function Counter() {    
     const [count,setCount] = useState(0)
+    const [value,setValue] = useState('')
     function increase() {
         setCount(count+1)
     }
-    
+    function increaseByValue() {
+        setCount(count + value)
+        setValue('')
+    }
   return (
     <div>
+        <input type="number" onChange={(e)=>{setValue(parseInt(e.target.value))}} 
+        value={value}  />
+        <button onClick={increaseByValue}>increase by value</button><br />
         <button onClick={()=>setCount(count-1)}>decrease</button>
         {count}
         <button onClick={increase}>increase</button>
@@ -21,6 +23,28 @@ function Counter() {
 }
 
 export default Counter 
+
+// function Counter() {
+//     //state is memory of comp
+//     //when state changes component re-renders
+//     //state is immutable u can not change state directly
+//     //use setter fn to change state 
+//     //it preserve the value b/w re-renders 
+//     const [count,setCount] = useState(0)
+//     function increase() {
+//         setCount(count+1)
+//     }
+    
+//   return (
+//     <div>
+//         <button onClick={()=>setCount(count-1)}>decrease</button>
+//         {count}
+//         <button onClick={increase}>increase</button>
+//     </div>
+//   )
+// }
+
+// export default Counter 
 
 //=============================
 // function Counter() {
