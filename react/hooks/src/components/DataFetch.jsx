cf5b63c0d8bd2a583dc00068bd2ef581b32e7c6a
ankur-upflairs <ankur.upflairs@gmail.com>
2025-08-01 10:50:21 +0530

    code pushed

---
+++ react/hooks/src/components/DataFetch.jsx
@@ -0,0 +1,29 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react' 
+
+function DataFetch() {
+    const [posts,setPosts] = useState([])
+    //effect is for managing side effects
+    //fnc => cannot be async function 
+    //dependency array => controlls the runing of effect 
+    //if dependency is empty than it only run once 
+    //there is a variable than when variable change it runs the effect 
+    // useEffect(fnc,[])
+    //npm i axios => get/post/put/delete
+    useEffect(()=>{
+        async function getData(){
+           let res = await axios.get('https://dummyjson.com/posts')
+            console.log(res.data)
+            setPosts(res.data.posts)
+        }
+        getData()
+    },[])
+    
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default DataFetch
